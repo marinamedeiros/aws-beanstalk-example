7b9223d5eea624645f157f2d8a40ebbee9f5e2ca
Marina Coutinho Medeiros <mcoutmed@gmail.com>
2020-07-16 16:51:55 -0300

    initial commit

---
+++ src/main/java/com/capstone/aws/aws/HelloController.java
@@ -0,0 +1,13 @@
+package com.capstone.aws.aws;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+    public String hello(){
+        return "Hello Capstone";
+    }
+
+}
---
+++ src/test/java/com/capstone/aws/aws/AwsElasticBeanstalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.aws.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstalkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
