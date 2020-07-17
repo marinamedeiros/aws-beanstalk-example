89c16ab310a70a4adc278084adb266ad599c0ed7
Marina Coutinho Medeiros <mcoutmed@gmail.com>
2020-07-16 22:32:29 -0300

    added getmapping in hellocontroller class

--- src/main/java/com/capstone/aws/aws/HelloController.java
+++ src/main/java/com/capstone/aws/aws/HelloController.java
@@ -1,4 +1,5 @@
 package com.capstone.aws.aws;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -6,8 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @GetMapping
     public String hello(){
-        return "Hello Capstone";
+        return "Hello Capstone!";
     }
 
 }
