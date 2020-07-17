0b14ed84d5febee86212756937bf6bef11c9d960
Marina Coutinho Medeiros <mcoutmed@gmail.com>
2020-07-16 22:43:10 -0300

    changes to pom

--- src/main/java/com/capstone/aws/aws/AwsElasticBeanstalkExampleApplication.java
+++ src/main/java/com/capstone/aws/aws/AwsElasticBeanstalkExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AwsElasticBeanstalkExampleApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AwsElasticBeanstalkExampleApplication.class, args);
 	}
 
